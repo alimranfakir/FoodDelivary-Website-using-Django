09f1562d67142d3ea060b11048d4e3a2acfe6b60
AL IMRAN FAKIR <48122690+alimranfakir@users.noreply.github.com>
2022-11-20 20:10:25 +0600

    update contact page

--- P_project1/view.py
+++ P_project1/view.py
@@ -11,8 +11,7 @@ def aboutPage(request):
     return render(request,"about.html")
 
 def blogPage(request):
-    return render(request,"blog.html")
-def contactPage(request):
+   
     return render(request,"contact.html")
 def menuPage(request):
     return render(request,"menu.html")
@@ -20,3 +19,38 @@ def productsPage(request):
     return render(request,"products.html")
 def reviewPage(request):
     return render(request,"review.html")
+
+def contactPage(request):
+    data={}
+    
+    
+    try:
+        if request.method=="POST":
+            
+            name = str(request.POST['userName'])
+            email = str(request.POST['useremail'])
+            number = int(request.POST['userNumber'])
+            
+
+            data={
+                    'name': name,
+                    'email': email,
+                    'number': number,
+                    'output': name,
+            }
+            return HttpResponseRedirect('/about/')
+
+    except:
+        pass
+    return render(request,"contact.html",data)
+
+   # def contactPage(request):
+    
+   # try:
+       # name = str(request.GET['userName'])
+       # email = str(request.GET['useremail'])
+       # number = int(request.GET['userNumber'])
+
+  #  except:
+     #   pass
+   # return render(request,"contact.html", {'output':name})
